47ec094e65e6d09f9b4849ff5c343a0e7959cf58
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-02-22 21:04:37 +0200

    build: config files for deploy to gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
+import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,7 +9,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/ravdevs_task',
+		}
 	}
 };
 
