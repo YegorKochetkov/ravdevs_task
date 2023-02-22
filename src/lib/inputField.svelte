e60ffc3a9ad77d1c6f577d3acf3caf2ed29f68c9
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-02-22 20:55:38 +0200

    feat: add input field for form

---
+++ src/lib/inputField.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let value: FormDataEntryValue | string | number;
+	export let label: string;
+</script>
+
+<label class="label">
+	<span>{label}:</span>
+	<input class="input" bind:value {...$$restProps} />
+</label>
+
+<style lang="scss">
+	.label {
+		display: flex;
+		flex-direction: column;
+		text-align: left;
+	}
+
+	.input {
+		display: block;
+		padding: 0.5rem;
+		margin-top: 0.5rem;
+		border-width: 1px;
+		border-radius: 0.25rem;
+	}
+</style>
